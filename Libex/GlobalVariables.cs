16eebc5efddb3d96cbcff44c5ff83a10bc69d358
Ouss <ozoubia@gmail.com>
2018-08-29 19:48:33 +0100

    added the menu cursor

---
+++ Libex/GlobalVariables.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Libex
+{
+    class GlobalVariables
+    {
+        public static TabControl tbControl;
+        
+    }
+}
--- Libex/MainWindow.xaml.cs
+++ Libex/MainWindow.xaml.cs
@@ -48,19 +48,27 @@ namespace Libex
 
         //max application button click
         private void maxAppBtn_Click(object sender, RoutedEventArgs e)
+        {           
+            this.WindowState = WindowState.Maximized;
+            maxAppBtn.Visibility = Visibility.Collapsed;
+            restoreAppBtn.Visibility = Visibility.Visible;
+        }
+        //show the restore down button on top of the window when it is maximized without the max button
+        private void Window_StateChanged(object sender, EventArgs e)
         {
-            if (this.WindowState == WindowState.Maximized )
+            if (this.WindowState == WindowState.Maximized)
             {
-                maxAppBtn.ToolTip = "Maximize";
-                this.WindowState = WindowState.Normal;
+                restoreAppBtn.Visibility = Visibility.Visible;
+                maxAppBtn.Visibility = Visibility.Collapsed;
             }
-            else
-            {
-                maxAppBtn.ToolTip = "Minimize";
-                this.WindowState = WindowState.Maximized;
-            }
-           
+        }
 
+        //restore minimize window
+        private void restoreAppBtn_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Normal;
+            restoreAppBtn.Visibility = Visibility.Collapsed;
+            maxAppBtn.Visibility = Visibility.Visible;
         }
 
         //turns the open menu button unchecked 
@@ -68,8 +76,7 @@ namespace Libex
         {
             openMenuBtn.IsChecked = false;
         }
-
-        
+       
         //left mouse clicked on the top color zone event
         private void ColorZone_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
@@ -82,14 +89,10 @@ namespace Libex
             this.WindowState = WindowState.Minimized;
         }
 
-
-
-
         private void tealAmberBtn_Click(object sender, RoutedEventArgs e)
         {
             this.Resources.MergedDictionaries.Clear();
-            AddResourceDictionary("Resources/TealAmberTheme.xaml");
-            
+            AddResourceDictionary("Resources/TealAmberTheme.xaml");            
         }
 
         private void blueGreyBtn_Click(object sender, RoutedEventArgs e)
@@ -110,5 +113,34 @@ namespace Libex
             ResourceDictionary resourceDictionary = Application.LoadComponent(new Uri(source, UriKind.Relative)) as ResourceDictionary;
             this.Resources.MergedDictionaries.Add(resourceDictionary);
         }
+
+        private void usercontrolShowBtn_Click(object sender, RoutedEventArgs e)
+        {
+            userControlGrid.Children.Clear();
+            TestUserControl UC1 = new TestUserControl();
+            userControlGrid.Children.Add(UC1);
+            UC1.Visibility = System.Windows.Visibility.Visible;
+        }
+        //public TabControl tbControl;
+        private void myTab_Loaded(object sender, RoutedEventArgs e)
+        {
+            GlobalVariables.tbControl = (sender as TabControl);
+        }
+
+        //method that shows which menu item is selected
+        private void MenuList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            int listItemSelectedIndex = MenuList.SelectedIndex;
+            MoveCursorMenu(listItemSelectedIndex);
+        }
+
+        //method that moves the cursor on the menu based on selection 
+        private void MoveCursorMenu(int listItemSelectedIndex)
+        {
+            //transitioningContentSlide.OnApplyTemplate();
+            MenuCursor.Margin = new Thickness(0, (117.5 + (50 * listItemSelectedIndex)), 0, 0);
+        }
+
+        
     }
 }
