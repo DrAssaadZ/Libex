80906003495a5c0c56045a0b7329290fb7595914
assaad <las9ayne_215@hotmail.com>
2018-08-29 15:22:48 +0200

    craeting the main classes

---
+++ Libex/SBook.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace Libex
+{
+    class SBook
+    {
+        int SBookId;
+        string SBookName;
+        string SBookISBN;
+        int SBookEdition;
+        int SBookPageNum;
+        string SBookAuthor;
+        int SBookRating;
+        string SBookAudience;
+        string SBookCopyRightsHolder;
+        string SBookEditor;
+        string SbookGenre;
+        float SBookPrice;
+        string SBookLang;
+        string SBookIllustrator;
+        BitmapImage SBookCover;
+
+        public SBook()
+        {
+
+        }
+    }
+}
---
+++ Libex/Sell.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Libex
+{
+    class Sell
+    {
+        int SBookId;
+        string SBookName;
+        string SBookISBN;
+        string SbookGenre;
+        float SBookPrice;
+        string CAgePeriod;
+        DateTime SBookDateSell;
+
+        public Sell()
+        {
+
+        }
+    }
+}
