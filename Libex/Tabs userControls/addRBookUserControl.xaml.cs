41013aa648dd993c496ac77a736b7089507cfb33
assaad <las9ayne_215@hotmail.com>
2018-09-25 12:42:54 +0200

    adding somme conditions on insertion

--- Libex/Tabs userControls/addRBookUserControl.xaml.cs
+++ Libex/Tabs userControls/addRBookUserControl.xaml.cs
@@ -29,12 +29,79 @@ namespace Libex.Tabs_userControls
 
         private void addBookBtn_Click(object sender, RoutedEventArgs e)
         {
+            if (bookNameBox.Text.Length < 1 || ISBNBox.Text.Length < 1 || editionYearBox.Text.Length < 4 || authorBox.Text.Length < 2 || nbrPagesBox.Text.Length < 1)
+            {
+                if (bookNameBox.Text.Length < 1)
+                {
+                    hint1.Text = "Name is empty or too short";
+                }
+                else
+                {
+                    hint1.Text = "";
+                }
+                if (authorBox.Text.Length < 2)
+                {
+                    hint2.Text = "Author is empty or too short";
+                }
+                else
+                {
+                    hint2.Text = "";
+                }
+                if (ISBNBox.Text.Length < 1)
+                {
+                    hint3.Text = "ISBN is empty or too short";
+                }
+                else if (!int.TryParse(ISBNBox.Text, out int x))
+                {
+                    hint3.Text = "ISBN should be a number";
+                }
+                else if (nbrPagesBox.Text.Length < 1)
+                {
+                    hint3.Text = "Enter number of pages";
+                }
+                else if (!int.TryParse(nbrPagesBox.Text, out int x2))
+                {
+                    hint3.Text = "NUMBER of pages should be a NUMBER";
+                }
+                else
+                {
+                    hint3.Text = "";
+                }
+                if (editionYearBox.Text.Length < 1)
+                {
+                    hint4.Text = "Year of edition  is empty or too short";
+                }
+                else if (!int.TryParse(ISBNBox.Text, out int x))
+                {
+                    hint4.Text = "year should be a number";
+                }
+                else
+                {
+                    hint4.Text = "";
+                }
+                if (priceBox.Text.Length < 1)
+                {
+                    hint5.Text = "price is empty or too short";
+                }
+                else if (!float.TryParse(ISBNBox.Text, out float x))
+                {
+                    hint5.Text = "price should be a number";
+                }            
+                else
+                {
+                    hint3.Text = "";
+                }
+            }
+            else
+            {
             confirmSnack.IsActive = true;
             DispatcherTimerConfirmSnack();
 
-            RBook obj = new RBook(bookNameBox.Text, ISBNBox.Text, int.Parse(editionYearBox.Text), int.Parse(nbrPagesBox.Text), bookAuthorBox.Text, AudienceBox.Text, copyrightHolderBox.Text, editorBox.Text
+            RBook obj = new RBook(bookNameBox.Text, ISBNBox.Text, int.Parse(editionYearBox.Text), int.Parse(nbrPagesBox.Text), authorBox.Text, AudienceBox.Text, copyrightHolderBox.Text, editorBox.Text
                                     , genreBox.Text, float.Parse(priceBox.Text), languageBox.Text, illustratorBox.Text, BasicRatingBar.Value, coverContainer.Source, aboutBox.Text);
             obj.insertRentBook();
+            }
+
         }
         private void DispatcherTimerConfirmSnack()
         {
